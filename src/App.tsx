33caa52ed5518015fe6439b4e7d08465a2e753d5
Akhilesh Upadhyay <umapati4381@gmail.com>
2023-09-19 01:43:36 +0530

    part 4 : useState advanced

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import {useState} from 'react'
-import Box from "./Box";
+import { useState, FormEvent } from "react";
 
+// ex form handling with useState
+
+interface Person {
+  name: string;
+  age: number;
+}
 function App() {
-  const [value,setValue]=useState<string>("")
+  const [user, setUser] = useState<Person>(); //at start user is undefined
+  // to get type of e -> onSubmit try {(e)=>{}}
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
     <>
-      <Box label="name" value={value} setter={setValue} />
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={user?.name || ""} //Warning: A component is changing an uncontrolled input to be controlled.
+          placeholder="name"
+          onChange={(e) => {
+            // prev! -> to remove error because user can be undefined
+            setUser((prev) => ({ ...prev!, name: e.target.value }));
+          }}
+        />
+        <input
+          type="number"
+          value={user?.age || ""} //Warning: A component is changing an uncontrolled input to be controlled.
+          placeholder="age"
+          onChange={(e) => {
+            // prev! -> to remove error because user can be undefined
+            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }));
+          }}
+        />
+        <button type="submit">submit</button>
+      </form>
     </>
   );
 }
